75fa0290dbfec9026067430d07a43e7e871e3239
obielson <gabriel.sousa@estudante.ifms.edu.br>
2019-11-24 21:24:33 -0400

    comentários

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var banco = [];
+var banco = []; // declaração do banco global
 
-function registro(){
+function registro(){ // função para guardar os dados do form
     let user = new User();
     user.entrada(nome.value, tel.value, email.value, cpf.value, desc.value);
     let dados = new bancodedados();
     dados.save(user);
 }
 
-function search(cpf){
+function search(cpf){ //função para buscar dados do banco
     let dados = new bancodedados();
     dados.search(cpf);
 }
